8df68674ebf8a095e0cf069e901aa1cdb255168b
SrivaniKunchala <srivanikunchala341@gmail.com>
2022-07-15 15:15:16 +0530

    Modules

--- Modules.js
+++ Modules.js
@@ -4,3 +4,7 @@ console.log(access);
 //Object Destructuring
 const {name} = require('./Objects').student1;
 console.log(name);
+const sum = require('./Objects');
+const res = sum.add(2,4);
+console.log(res);
+
